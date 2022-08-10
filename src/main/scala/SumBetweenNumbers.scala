b2f4e5a0a7aa36a43756309ad41f4bd37699b37a
edwardosei <edward.osei.nyarko@gmail.com>
2022-08-10 12:58:29 +0000

    Fix stack overflow bug in recursion method.

--- src/main/scala/SumBetweenNumbers.scala
+++ src/main/scala/SumBetweenNumbers.scala
@@ -2,9 +2,9 @@ object SumBetweenNumbers extends App {
   def sumNumbers(firstNumber: Int, secondNumber: Int): Int = {
     if (firstNumber.equals(secondNumber)) throw IllegalArgumentException("integers args should not have the same value")
     val biggerNumber = if (firstNumber < secondNumber)  secondNumber else firstNumber
-    val smallerNumber = if (secondNumber < firstNumber)  firstNumber else secondNumber
-    val currentSum = biggerNumber - 1
-    if (currentSum.equals(smallerNumber)) currentSum + 1
-    else currentSum + sumNumbers(smallerNumber, biggerNumber)
+    val smallerNumber = if (secondNumber > firstNumber)  firstNumber else secondNumber
+    val currentDigit = biggerNumber - 1
+    if ((currentDigit - 1).equals(smallerNumber)) currentDigit
+    else currentDigit + sumNumbers(smallerNumber, biggerNumber - 1)
   }
 }
